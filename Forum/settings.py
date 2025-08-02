c6bb6577b2150b5186f6a43f877e52799c6ff4c0
Beksultan <sharshenbekov.tech@gmail.com>
2025-08-02 12:10:17 +0600

    создал приложение webapp

--- Forum/settings.py
+++ Forum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'webapp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
