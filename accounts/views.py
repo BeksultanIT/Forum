1020e7c5fe2b4c5b455e1fea78a8665c55118130
Beksultan <sharshenbekov.tech@gmail.com>
2025-08-02 17:31:41 +0600

    добавил profile для пользователей

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
+from accounts.models import Profile
 
 # Create your views here.
 User = get_user_model()
@@ -15,6 +17,7 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        Profile.objects.create(user=user)
         login(self.request, user)
         return redirect(self.get_success_url())
 
@@ -25,3 +28,9 @@ class RegisterView(CreateView):
         if not next:
             next = reverse("webapp:index")
         return next
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'user_profile.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 12
