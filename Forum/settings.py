627c9f4a5289a365205269f5e15d177fbe3e387f
Beksultan <sharshenbekov.tech@gmail.com>
2025-08-02 14:36:54 +0600

    Добавил login и logout

--- Forum/settings.py
+++ Forum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'webapp/templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
